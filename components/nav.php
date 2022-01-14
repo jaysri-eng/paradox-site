f9bc8d5374a60a97a5f39ae1752163c8e5ed79e6
jaysri-eng <jayanthsrinivasan1011@gmail.com>
2022-01-14 21:05:14 +0530

    first commit

---
+++ components/nav.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Paradox Inc</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/Paradox_Website/home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Paradox_Website/products.php">Products</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
---
+++ products.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Paradox Inc</title>
+  </head>
+  <body style="background-color: grey;">
+    <?php require 'components/nav.php'?>  
+    <div>
+  <div class="d-md-flex my-md-5 mx-md-5 ps-md-5 px-md-5">
+    <div class="bg-light me-md-4 pt-3 px-5 pt-md-4 px-md-5 text-center overflow-hidden border border-primary">
+      <div class="my-3 p-3">
+        <h2 class="display-5" style="color: black; font-size: 20px">Basics of Sign Language</h2>
+      </div>
+      <div class="bg-dark shadow-sm mx-auto text-center" style="width: 100%; height: 100%; border-radius: 25px 25px 40px 40px;">
+        <p style="color: white;" class="text-center">
+        Over 5% of the world's population have disabling hearing loss. 
+        Till 2020, only 17% of those population have had accent to a hearing aid. 
+        Those who can't afford hearing aid devices depend on Sign Language for their everyday life which sadly, is not ubiquitous among the educated. 
+        Learning Sign Language helps bridge the gap between communication and understanding for people with hearing loss. 
+        Sign Language app by Paradox Inc. offers to educate you on the same, free of cost.
+        </p>
+      </div>
+    </div>
+  </div>
+  </div></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+  <footer class="mt-5">
+  <div class="text-center p-5" style="color: black;">
+    ©2022 Paradox Inc
+  </div>
+  </footer>
+</html>
