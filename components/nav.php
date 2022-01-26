7d82a64db792cd1e857cd145debf860f4b10e02f
jaysri-eng <jayanthsrinivasan1011@gmail.com>
2022-01-26 20:52:05 +0530

    company logo

--- components/nav.php
+++ components/nav.php
@@ -1,20 +1,23 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:darkorange;">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button style="color:black; background-color:orange" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span style="color: orange;" class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Paradox Inc</a>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div>
+      <a>Paradox Inc</a>
+    </div>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
+      <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/Paradox_Website/index.php">Home</a>
+          <a class="nav-link active" style="color: black;" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/Paradox_Website/products.php">Products</a>
+          <a class="nav-link" href="products.php" style="color: black;">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="color: grey;">Services</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-
-
--- index.php
+++ index.php
@@ -1,35 +1,48 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Paradox Inc</title>
-  </head>
-  <body style="background-color: grey">
-    <?php require 'components/nav.php'?>  
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <title>Paradox Inc</title>
+</head>
+<style>
+  .grad {
+    background-image: linear-gradient(to bottom right, white, white);
+  }
+</style>
+<div class="grad">
+
+  <body style="background-color: white">
+    <?php require 'components/nav.php' ?>
     <div class="px-5 mt-5 row" style="width: 100%;">
-    <div class="col">
-      <br/>
-      <h4 class="text-center" style="color: white;">SaaS with sass!</h4>
-    <p class="px-5 text-center" style="font-size: 18px; color: white; width: 100%">
-    A fledgling B2C SaaS startup in the works that is the brainchild of 3 youngsters. 
-    Paradox has a strong, service-minded approach to provide hosted services and app development programs. 
-    Paradox considers the client's needs and caters to them in a radical yet personal way. 
-    The company specializes in automating internal functions within businesses and is directly engaged to the marketplace. 
-    As it offers marketing, software development, client engagement and sales optimization related services, your deeds are now easy with Paradox in your hands.
-    </p>
+      <div class="col-lg-6 image">
+        <img src="assets/Paradox .png" />
+      </div>
+      <div class="col-lg-6">
+        <br />
+        <h4 class="text-center" style="color: black;">SaaS with sass!</h4>
+        <p class="px-5 text-center" style="font-size: 18px; color: black; width: 100%">
+          A fledgling B2C SaaS startup in the works that is the brainchild of 3 youngsters.
+          Paradox has a strong, service-minded approach to provide hosted services and app development programs.
+          Paradox considers the client's needs and caters to them in a radical yet personal way.
+          The company specializes in automating internal functions within businesses and is directly engaged to the marketplace.
+          As it offers marketing, software development, client engagement and sales optimization related services, your deeds are now easy with Paradox in your hands.
+        </p>
+      </div>
     </div>
-  </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
-  <footer class="mt-5">
-  <div class="text-center p-5" style="color: black;">
-    ©2022 Paradox Inc
-  </div>
+  <footer class="mt-5 fw-bold">
+    <div class="text-center p-5" style="color: darkorange;">
+      ©2022 Paradox Inc
+    </div>
   </footer>
+</div>
+
 </html>
